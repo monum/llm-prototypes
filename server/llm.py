5fa75a3476771e1622b6b3a8b8ca23c28c77a1d1
Yuntong Fu <yuntongf@sas.upenn.edu>
2023-08-09 14:10:10 +0800

    azure temp

--- server/llm.py
+++ server/llm.py
@@ -8,6 +8,8 @@ from langchain.embeddings import OpenAIEmbeddings
 from pathlib import Path
 import openai
 import config
+import json
+import requests
 
 from langchain.agents import Tool, AgentExecutor, initialize_agent
 from langchain.agents.loading import AGENT_TO_CLASS, load_agent
@@ -22,43 +24,44 @@ from llama_index.node_parser import SimpleNodeParser
 from llama_index import VectorStoreIndex, SimpleDirectoryReader, load_index_from_storage
 from llama_index.storage.storage_context import StorageContext
 
+# Azure imports
+from langchain.vectorstores.azuresearch import AzureSearch
+# from dotenv import load_dotenv
 '''
 to run script: 
 python /Users/omgitsmonday/projects/llm-prototypes/server/llm.py
 '''
 
-# api set up
-# NOTE: for local testing only
+# env var set up
+
+##### OpenAI set up #####
+# load_dotenv()
 os.environ['OPENAI_API_KEY'] = config.OPENAI_API_KEY # remove key when push to remote
 openai.api_key = config.OPENAI_API_KEY 
+# openai.api_type = "azure"
+# openai.api_base = "YOUR_OPENAI_ENDPOINT"
+# openai.api_version = "2023-05-15"
+# openai.api_key = "YOUR_OPENAI_API_KEY"
+
+#### set up vector store ####
+model: str = "text-embedding-ada-002"
+vector_store_address: str = config.AZURE_SEARCH_ENDPOINT
+vector_store_password: str = config.AZURE_SEARCH_ADMIN_KEY
+index_name: str = "azureblob-index"
+embeddings: OpenAIEmbeddings = OpenAIEmbeddings(model=model, chunk_size=1)
+vector_store: AzureSearch = AzureSearch(
+    azure_search_endpoint=vector_store_address,
+    azure_search_key=vector_store_password,
+    index_name=index_name,
+    embedding_function=embeddings.embed_query,
+)
 
 # set up flask app
 app = Flask(__name__)
 CORS(app)
 
-# set up pinecone vector store
-import pinecone 
-from langchain.vectorstores import Pinecone
 from langchain.document_loaders import TextLoader
 from langchain.text_splitter import CharacterTextSplitter
-
-# initialize pinecone
-pinecone.init(
-    api_key=config.PINECONE_API_KEY,  # find at app.pinecone.io
-    environment=config.PINECONE_ENVIRONMENT
-)
-# initialize pinecone index (treat as a table)
-index_name = "llm-prototypes"
-if index_name not in pinecone.list_indexes():
-    # we create a new index
-    pinecone.create_index(
-        name=index_name,
-        metric='cosine',
-        dimension=1536  # 1536 dim of text-embedding-ada-002
-    )
-pinecone_index = pinecone.GRPCIndex(index_name)
-print(pinecone_index.describe_index_stats())
-pinecone.describe_index(index_name)
 
 ####################################### routes ########################################
 
@@ -69,19 +72,23 @@ def query_agent():
     query = request_data['question']
     if query is None:
         return "No text found:(", 400
-
     print("User query: " + query)
+
+    # sources
     docs = get_similar_docs(query)
     metadata = [doc.metadata for doc in docs]
-    response = agent({"question":query}, return_only_outputs=True)
-    print(response)
+
+    # response
+    # response = agent({"question":query}, return_only_outputs=True)
+    # print(response)
+    
     res = {
-        "answer": response['answer'],
-        "sources": response['sources'],
-        "id": [meta['id'] for meta in metadata],
-        "label": [meta['label'] for meta in metadata],
-        "description": [meta['description'] for meta in metadata],
-        "date": [meta['date'] for meta in metadata]
+        # "answer": response['answer'],
+        # "sources": response['sources'],
+        # "id": [meta['id'] for meta in metadata],
+        # "label": [meta['label'] for meta in metadata],
+        # "description": [meta['description'] for meta in metadata],
+        # "date": [meta['date'] for meta in metadata]
     }
     return res, 200
 
@@ -103,7 +110,7 @@ def upload_file():
         
         document = SimpleDirectoryReader(input_files=[filepath]).load_data()[0]
         source = label + '/' + document.extra_info['file_name']
-        build_index([document], "./store", source, label, description)
+        build_vectors([document], "./store", source, label, description)
     except Exception as e:
         print(e)
         # cleanup temp file
@@ -131,7 +138,7 @@ def upload_wiki():
         print(page_name)
         document = loader.load_data(pages=[page_name])
         # print(document[0])
-        build_index(document, './store', source, label, description)
+        build_vectors(document, './store', source, label, description)
     except Exception as e:
         print(e)
         return "Error: {}".format(str(e)), 500
@@ -149,7 +156,7 @@ def upload_url():
         loader = BeautifulSoupWebReader()
         documents = loader.load_data(urls=[source])
 
-        build_index(documents, './store', source, label, description)
+        build_vectors(documents, './store', source, label, description)
     except Exception as e:
         print(e)
         return "Error: {}".format(str(e)), 500
@@ -182,57 +189,27 @@ def retrieve_doc():
 
 
 ##################################### index building ######################################
-def initialize_index():
-    ''' 
-    use LlamaIndex as a query tool, augment the model with specific data
-    '''
-    return build_index([], "./store")
         
-def build_index(documents, persist_dir, source="Uploaded file", label="General", description="Boston government document"):
-    # pine cone store
-    # text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=100)
-    # docs = text_splitter.split_documents(documents)
-    # embeddings = OpenAIEmbeddings()
-    # docsearch = Pinecone.from_documents(docs, embeddings, index_name="llm-prototypes")
-
+def build_vectors(documents, persist_dir, source="Uploaded file", label="General", description="Boston government document"):
     # create storage context using default stores
-    from llama_index.vector_stores import PineconeVectorStore
     import datetime
 
-    # vector store in pinecone
-    vector_store = PineconeVectorStore(
-        pinecone_index=pinecone_index,
-        add_sparse_vector=True,
+    vector_store: AzureSearch = AzureSearch(
+        azure_search_endpoint=vector_store_address,
+        azure_search_key=vector_store_password,
+        index_name=index_name,
+        embedding_function=embeddings.embed_query,
     )
-    storage_context = StorageContext.from_defaults(
-        docstore=SimpleDocumentStore(),
-        vector_store=vector_store,
-        index_store=SimpleIndexStore(),
-    )
-    index = None  
     if (documents is not None):
-        # create parser and parse document into nodes 
-        parser = SimpleNodeParser()
-        nodes = parser.get_nodes_from_documents(documents)
-        for node in nodes:
-            if (node.extra_info is None):
-                node.extra_info = {}
-            node.extra_info['source'] = source
-            node.extra_info['label'] = label
-            node.extra_info['description'] = description
-            node.extra_info['date'] = str(datetime.date.today())
-
-        # create (or load) docstore and add nodes
-        # storage_context.docstore.add_documents(nodes)
-
-        # build index
-        index = VectorStoreIndex(nodes, storage_context=storage_context)
-    else: 
-        index = VectorStoreIndex([], storage_context=storage_context)
-
-    # save index
-    index.storage_context.persist(persist_dir=persist_dir)
-
+        text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=100)
+        docs = text_splitter.split_documents(documents)
+        for document in docs:
+            document.extra_info['source'] = source
+            document.extra_info['label'] = label
+            document.extra_info['description'] = description
+            document.extra_info['date'] = str(datetime.date.today())
+    
+    vector_store.add_documents(documents=docs)
 
 ##################################### tool building ######################################
 def build_chain(): # https://python.langchain.com/en/latest/modules/agents/tools/custom_tools.html
@@ -247,25 +224,33 @@ def build_chain(): # https://python.langchain.com/en/latest/modules/agents/tools
     )
 
     # vector store
-    vectorstore = Pinecone(
-        pinecone.Index("llm-prototypes"), 
-        embed.embed_query, 
-        "text"
-    )
+    # vector_store: AzureSearch = AzureSearch(
+    #     azure_search_endpoint=vector_store_address,
+    #     azure_search_key=vector_store_password,
+    #     index_name=index_name,
+    #     embedding_function=embeddings.embed_query,
+    # )
     
-    from langchain.chains import RetrievalQAWithSourcesChain
-    agent = RetrievalQAWithSourcesChain.from_chain_type(
-        llm=build_llm(),
-        chain_type="stuff",
-        retriever=vectorstore.as_retriever()
-    )
+    # from langchain.chains import RetrievalQAWithSourcesChain
+    # agent = RetrievalQAWithSourcesChain.from_chain_type(
+    #     llm=build_llm(),
+    #     chain_type="stuff",
+    #     retriever=vector_store.as_retriever()
+    # )
 
 def get_similar_docs(query):
-    embeddings = OpenAIEmbeddings()
-
-    docsearch = Pinecone.from_existing_index(index_name, embeddings)
-
-    docs = docsearch.similarity_search(query)
+    vector_store: AzureSearch = AzureSearch(
+        azure_search_endpoint=vector_store_address,
+        azure_search_key=vector_store_password,
+        index_name=index_name,
+        embedding_function=embeddings.embed_query,
+    )
+    docs = vector_store.similarity_search(
+        query=query,
+        k=3,
+        search_type="similarity",
+    )
+    print(docs)
     return docs
 
 def build_llm():
@@ -275,6 +260,6 @@ def build_llm():
     )
 
 if __name__ == "__main__":
-    build_index([], "./store")
+    build_vectors([], "./store")
     build_chain()
     app.run(host="0.0.0.0", port=5601)
